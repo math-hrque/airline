b038f547e69fdf54155d6edc568b99a471d2f2ba
Yasmin Calderon <yasmiin.allanny@gmail.com>
2024-10-24 18:45:46 -0300

    Inicio R04 - consultar reserva cliente

--- ms-reserva/reserva/src/main/java/br/com/reserva/reserva/controllers/conta_r/ReservaRController.java
+++ ms-reserva/reserva/src/main/java/br/com/reserva/reserva/controllers/conta_r/ReservaRController.java
@@ -11,7 +11,46 @@ import br.com.reserva.reserva.services.conta_r.ReservaRService;
 public class ReservaRController {
 
     @Autowired
-    ReservaRService reservaRService;
+    ReservaRService reservaService;
 
+  // código em construção!!
+  // @GetMapping("/consultar-reserva/{id}")
+  //   public ResponseEntity<Object> consultarReservaById(@PathVariable("id") String id) {
+  //       try {
+  //           Object reservaConsultada = reservaService.consultarReservaById(id);
+  //           return ResponseEntity.status(HttpStatus.OK).body(reservaConsultada);
+  //       } catch (ReservaNaoExisteException e) {
+  //           return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+  //       } catch (Exception e) {
+  //           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+  //       }
+  //   }
+  
+  // @GetMapping("/consultar-reserva/")
+  //   public ResponseEntity<Object> consultarTodasReservas() {
+  //       try {
+  //           // lista todas as reservas
+  //           Object reservaConsultada = reservaService.consultarReservas();
+  //           return ResponseEntity.status(HttpStatus.OK).body(reservaConsultada);
+  //       } catch (ReservaNaoExisteException e) {
+  //           return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+  //       } catch (Exception e) {
+  //           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+  //       }
+  //   }
+
+  // // em construção
+  // @PostMapping("/criar-reserva/")
+  //   public ResponseEntity<Object> consultarTodasReservas(@) {
+  //       try {
+  //           // lista todas as reservas
+  //           Object reservaConsultada = reservaService.consultarReservas();
+  //           return ResponseEntity.status(HttpStatus.OK).body(reservaConsultada);
+  //       } catch (ReservaNaoExisteException e) {
+  //           return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+  //       } catch (Exception e) {
+  //           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+  //       }
+  //   }
 
 }
