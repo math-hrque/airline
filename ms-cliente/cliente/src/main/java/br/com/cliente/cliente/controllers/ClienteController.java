4d170fa98923f68ec10cb88c2cce7031c57c0df6
Julio Yamaguchi <julioyamaguchi@ufpr.br>
2024-11-07 18:53:36 -0300

    Endpoint Cosultar Milhas

--- ms-cliente/cliente/src/main/java/br/com/cliente/cliente/controllers/ClienteController.java
+++ ms-cliente/cliente/src/main/java/br/com/cliente/cliente/controllers/ClienteController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @RestController
 @RequestMapping(value = "/ms-cliente")
 public class ClienteController {
@@ -27,26 +26,35 @@ public class ClienteController {
 
     @GetMapping("/listar-milhas/{idCliente}")
     public ResponseEntity<ClienteMilhasDto> consultarExtratoMilhas(@PathVariable("idCliente") Long idCliente) {
-        try{
-        ClienteMilhasDto extrato = clienteService.consultarExtratoMilhas(idCliente); 
-        return ResponseEntity.ok(extrato);
-        } catch (ClienteNaoExisteException e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }       
-    }
-
-    @PostMapping("/comprar-milhas")
-    public ResponseEntity<MilhasDto> comprarMilhas(@RequestParam("idCliente") Long idCliente,
-    @RequestParam("quantidadeMilhas") int quantidadeMilhas, @RequestBody MilhasDto milhas){
-        milhas.setQuantidadesMilhas(quantidadeMilhas);
-        try{
-            clienteService.comprarMilhas(milhas, idCliente);
-            return ResponseEntity.status(HttpStatus.CREATED).body(milhas);
-        } catch (ClienteNaoExisteException e){
+        try {
+            ClienteMilhasDto extrato = clienteService.consultarExtratoMilhas(idCliente);
+            return ResponseEntity.ok(extrato);
+        } catch (ClienteNaoExisteException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 
-    
+    @PostMapping("/comprar-milhas")
+    public ResponseEntity<MilhasDto> comprarMilhas(@RequestParam("idCliente") Long idCliente,
+            @RequestParam("quantidadeMilhas") int quantidadeMilhas, @RequestBody MilhasDto milhas) {
+        milhas.setQuantidadesMilhas(quantidadeMilhas);
+        try {
+            clienteService.comprarMilhas(milhas, idCliente);
+            return ResponseEntity.status(HttpStatus.CREATED).body(milhas);
+        } catch (ClienteNaoExisteException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    // @GetMapping("/listar-milhas/{idCliente}")
+    // public ResponseEntity<ClienteMilhasDto>
+    // consultarExtratoMilhas(@PathVariable("idCliente") Long idCliente) {
+    // try {
+    // ClienteMilhasDto extrato = clienteService.consultarExtratoMilhas(idCliente);
+    // return ResponseEntity.ok(extrato);
+    // } catch (ClienteNaoExisteException e) {
+    // return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    // }
+    // }
 
 }
